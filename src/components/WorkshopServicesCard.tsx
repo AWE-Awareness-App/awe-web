15394dce0444786500d65a547ad429310d15f355
Matthieu Smith <matthieu.smith@arcticflamegames.com>
2025-03-24 11:19:29 -0400

    Fixed workshop section + updated readme for tailwind instructions

--- src/components/WorkshopServicesCard.tsx
+++ src/components/WorkshopServicesCard.tsx
@@ -18,22 +18,23 @@ const WorkshopServicesCard: React.FC<WorkshopServicesCardProps> = ({
     <div
       className={`rounded-xl p-6 ${bgColor} flex flex-col shadow-md h-full w-full ${className}`}
     >
-      {/* Title */}
-      <div>
-        <h3 className="text text-black h-9">{title}</h3>
+
+      <div className="flex-grow">
+        {/* Title */}
+        <h3 className="font-bold text-2xl text-blue-950 h-9">{title}</h3>
+        {/* Features */}
+        <ul className="space-y-2 p-2">
+          {features.map((feature, index) => (
+            <li key={index} className="flex items-center text-blue-950">
+              <FaPlus className="mr-2" />
+              {feature}
+            </li>
+          ))}
+        </ul>
       </div>
-      {/* Features */}
-      <ul className="space-y-2 p-2">
-        {features.map((feature, index) => (
-          <li key={index} className="flex items-center">
-            <FaPlus className="text-black mr-2" />
-            {feature}
-          </li>
-        ))}
-      </ul>
 
       {/* Session Apply Now */}
-      <div className="mt-4 mb-4">
+      <div className="mt-4 mb-4 flex justify-center">
         <button className="p-3 bg-blue-950 text-white rounded-lg w-[75%] hover:bg-blue-700 transition mt-4 center">
           Apply Now
         </button>
--- src/sections/WorkshopServicesSection.tsx
+++ src/sections/WorkshopServicesSection.tsx
@@ -36,45 +36,8 @@ const OrganizationWorkshopFeatures = [
 
 const WorkshopServicesSection: React.FC = () => {
   return (
-    <div className="grid grid-cols-3 md:grid-cols-3 gap-6 py-2 items-center justify-items-center">
-      <WorkshopServicesCard
-        className=""
-        title="1. Individual Workshops"
-        features={IndividualWorkshopsFeatures}
-        bgColor="bg-green-200"
-      />
-      <WorkshopServicesCard
-        className=""
-        title="2. Couples Workshops"
-        features={CouplesWorkshopsFeatures}
-        bgColor="bg-yellow-200"
-      />
-      <WorkshopServicesCard
-        className=""
-        title="3. Family Workshops"
-        features={FamilyWorkshopFeatures}
-        bgColor="bg-pink-200"
-      />
-      <WorkshopServicesCard
-        className=""
-        title="4. Communities Workshops"
-        features={CommunitiesWorkshopFeatures}
-        bgColor="bg-yellow-200"
-      />
-      <WorkshopServicesCard
-        className=""
-        title="5. Organization Workshops"
-        features={OrganizationWorkshopFeatures}
-        bgColor="bg-pink-200"
-      />
-    </div>
-  );
-};
-
-export default WorkshopServicesSection;
-
-/*    <div className="">
-      <div className="grid grid-cols-3 md:grid-cols-3 gap-x-28 py-2 items-center justify-items-center">
+    <div className="flex flex-col gap-6 py-2">
+      <div className="grid grid-cols-3 gap-6 items-center justify-items-center">
         <WorkshopServicesCard
           className=""
           title="1. Individual Workshops"
@@ -93,19 +56,26 @@ export default WorkshopServicesSection;
           features={FamilyWorkshopFeatures}
           bgColor="bg-pink-200"
         />
+
       </div>
-      <div className="grid grid-cols-3 md:grid-cols-2 gap-x-28 py-2 items-center justify-items-center">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-1" />
         <WorkshopServicesCard
-          className=""
+          className="col-span-2"
           title="4. Communities Workshops"
           features={CommunitiesWorkshopFeatures}
           bgColor="bg-yellow-200"
         />
         <WorkshopServicesCard
-          className=""
+          className="col-span-2"
           title="5. Organization Workshops"
           features={OrganizationWorkshopFeatures}
           bgColor="bg-pink-200"
         />
+        <div className="col-span-1" />
       </div>
-    </div>; */
+    </div>
+  );
+};
+
+export default WorkshopServicesSection;
