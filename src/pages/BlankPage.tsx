669f804a3ecd5226a9f888ab7fbdfb6078006f26
antonyleungca <antonyleungca@gmail.com>
2025-03-26 00:19:42 -0600

    Init PopUpBox and LoginModal

--- src/pages/BlankPage.tsx
+++ src/pages/BlankPage.tsx
@@ -1,27 +1,49 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import PopUpBox from "../components/PopUpBox.tsx";
+import LoginModal from "../components/LoginModal.tsx";
 
 const BlankPage: React.FC = () => {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
       <button
-        data-modal-target="static-modal"
-        data-modal-toggle="static-modal"
+        className="block text-gray-700 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+        onClick={() => setIsLoginOpen(true)}
+      >
+        Login
+      </button>
+      <LoginModal
+        email="email"
+        password="password"
+        isOpen={isLoginOpen}
+        onClose={() => setIsLoginOpen(false)}
+      />
+      <button
+        data-modal-target="popup-modal"
+        data-modal-show="popup-modal"
+        data-modal-toggle="popup-modal"
         className="block text-gray-700 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         onClick={() => setIsOpen(true)}
       >
-        Login
+        PopUpBox
       </button>
 
-      {isOpen &&
+      <PopUpBox
+        title="Terms of Service"
+        description="Your terms content goes here."
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+
+      {/* {isOpen &&
         ReactDOM.createPortal(
           <PopUpBox title="jk" description="kk" />,
           document.body
-        )}
+        )} */}
     </div>
   );
 };
