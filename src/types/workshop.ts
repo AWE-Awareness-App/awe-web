3b329b4b26c4436720549409f85faea2a4fc2a70
Matthieu Smith <matthieu.smith@arcticflamegames.com>
2025-08-19 20:35:34 -0400

    merge updates

--- src/types/workshop.ts
+++
@@ -1,9 +0,0 @@
-import { Workshop } from '@generated/api';
-
-export interface WorkshopServicesCardProps {
-  workshop: Workshop;
-  className?: string;
-  bgColor?: string;
-  onPurchaseInitiated?: (workshop: Workshop) => Promise<void>;
-  onBookNow?: (workshop: Workshop) => void;
-}
